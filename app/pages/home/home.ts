76754e26e265633ef507cf4a52cacf16f6ce29b5
moncif khoulfi <moncef.khoulfi@gmail.com>
2016-08-17 18:36:17 +0200

    fix loader

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { FormGroup, REACTIVE_FORM_DIRECTIVES, FormBuilder, Validators } from '@angular/forms';
-import {NavController} from 'ionic-angular';
+import {NavController, LoadingController} from 'ionic-angular';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
 import {Todo} from '../dto/todo';
 import {TodoService} from '../../providers/todo-service/todo-service';
@@ -9,6 +9,7 @@ import { AngularFire, AuthProviders } from 'angularfire2';
 @Component({
   templateUrl: 'build/pages/home/home.html',
   pipes: [TranslatePipe],
+  providers: [TodoService],
   directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class HomePage implements OnInit {
@@ -16,16 +17,20 @@ export class HomePage implements OnInit {
   todos: Todo[];
   registerForm: FormGroup;
   user = {};
+  users = [];
   appName = 'Ionic App';
 
   constructor(private nav: NavController,
+    private loadingCtrl: LoadingController,
     private todoService: TodoService,
     private fb: FormBuilder,
     private fire: AngularFire) {
   }
 
   ngOnInit() {
-
+    /**
+     * auto login
+     */
     this.fire.auth.subscribe(user => {
       if (user) {
         this.user = user.auth;
@@ -34,6 +39,9 @@ export class HomePage implements OnInit {
       }
     });
 
+    /**
+     * init registration form
+     */
     this.registerForm = this.fb.group({
       fname: ['David', [Validators.required, Validators.minLength(5)]],
       lname: ['', Validators.required],
@@ -42,7 +50,29 @@ export class HomePage implements OnInit {
         zip: ['', [Validators.required, Validators.pattern('[0-9]{5}')]]
       })
     });
+
+    /**
+     * loading todo's
+     */
+  }
+
+
+  ionViewWillEnter () {
+    let loading = this.loadingCtrl.create({
+      spinner: 'dots',
+      content: 'Please wait...'
+    });
+
     this.todoService.load().then(todos => this.todos = todos);
+    loading.present();
+
+    setTimeout(() => {
+      loading.dismiss();
+    }, 1000);
+  }
+
+  save(): void {
+    this.users.push(this.registerForm.value);
   }
 
 
